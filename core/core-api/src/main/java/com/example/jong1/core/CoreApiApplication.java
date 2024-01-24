926d1d299b17c567e137fdfa6045dba778fb0b06
Jong1 <whddnjs822@gmail.com>
2024-01-24 13:51:13 +0900

    add : Lombok추가 및 잘못된 패키지 수정

--- core/core-api/src/main/java/com/example/jong1/core/CoreApiApplication.java
+++ core/core-api/src/main/java/com/example/jong1/core/CoreApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.jong1;
+package com.example.jong1.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
