84510b5575c043389326fe5c6bfc57018f22761d
Jong1 <whddnjs822@gmail.com>
2024-06-17 14:55:00 +0900

    Modify : Spring AI 그 한달사이 변경점.. - openAiChatClient가 사라짐. - mileStone레포에서 땡겨오면 OpenAiChatModel가 빈설정이 안되어있음.

--- core/core-api/src/main/java/com/example/jong1/core/api/ChatGptController.java
+++ core/core-api/src/main/java/com/example/jong1/core/api/ChatGptController.java
@@ -3,10 +3,10 @@ package com.example.jong1.core.api;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.openai.OpenAiChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +17,7 @@ import reactor.core.publisher.Flux;
 @RequiredArgsConstructor
 public class ChatGptController {
     // OpenAiChatClient
-    private final OpenAiChatClient chatClient;
+    private final OpenAiChatModel openAiChatModel;
 
     // @RequestParam을 사용하여 "message"라는 이름의 요청 파라미터를 String 타입의 message 변수에 바인딩합니다.
     // 메세지가 없을시 기본 값을 "안녕하세요"으로 설정하여, 요청에서 message 파라미터가 누락된 경우 기본값을 사용합니다.
@@ -27,7 +27,7 @@ public class ChatGptController {
         // "generation"이라는 키와 함께 결과를 Map 객체로 래핑하여 반환합니다.
         // 이 Map 객체는 JSON 형식으로 클라이언트에게 응답됩니다.
         log.error("{}", chat.getMessage());
-        return Map.of("generation", chatClient.call(chat.getMessage()));
+        return Map.of("generation", openAiChatModel.call(chat.getMessage()));
     }
 
     // 비동기 스트림 처리 방식
@@ -41,6 +41,6 @@ public class ChatGptController {
 
         Prompt prompt = new Prompt(new UserMessage(chat.getMessage()));
         log.error("Prompt.toString >>> {}", prompt.toString());
-        return chatClient.stream(prompt);
+        return openAiChatModel.stream(prompt);
     }
 }
